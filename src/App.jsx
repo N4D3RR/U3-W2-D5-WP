0f8d8b10df9e628008f74c703403bc941685ebaa
NAD3R <naderdeghaili@gmail.com>
2025-11-28 17:11:01 +0100

    fixed NavBar name for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
-import NavBar from "./components/Navbar"
+import NavBar from "./components/NavBar"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "leaflet/dist/leaflet.css"
 import Footer from "./components/Footer"
